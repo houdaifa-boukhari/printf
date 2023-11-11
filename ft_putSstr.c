6cf6bce6d373a88f6a1615a4e04ef6755259e937
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-11-11 17:42:35 +0100

    my_projects

--- ft_putSstr.c
+++ ft_putSstr.c
@@ -12,16 +12,18 @@ int	ft_putSstr(char *str)
 	int i;
 
 	i = 0;
+	if (!str)
+		return (ft_putstr('s', "(nil)"));
 	while (str[i])
 	{
 		if (str[i] >= 32 && str[i] <= 126)
 			ft_putchar(str[i]);
-		if (str[i] < 32 || str[i] >= 127)
+		else if (str[i] < 32 || str[i] >= 127)
 		{
 			write(1, "\\x0", 3);
 			ft_putnbr_hexa('X', (long)str[i]);
 		}
 		i++;
 	}
-	return (ft_strlen(str));
+	return (i);
 }
