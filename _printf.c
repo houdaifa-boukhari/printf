1fd51ed123b9f12a5885b6a1b41d5efd64aba520
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-11-12 12:42:19 +0100

    My project

--- _printf.c
+++ _printf.c
@@ -61,7 +61,7 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	i = 0;
 	len = 0;
-	if (!format || format[0] == '%' && !format[1])
+	if (!format ||(format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
