cc9414f9b0a8c3711d22f919e1f75114495ae9a5
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2023-11-12 14:44:48 +0100

    My project

--- ft_putshexa.c
+++ ft_putshexa.c
@@ -1,5 +1,23 @@
 #include "main.h"
+/**
+ * len2  - calcule len to nb
+ * @nb: nb to calcul
+ *
+ * Return: len
+ */
 
+int len2(long nb)
+{
+	int i;
+
+	i = 1;
+	while (nb >= 16)
+	{
+		nb /= 16;
+		i++;
+	}
+	return (i);
+}
 /**
  * ft_putshexa  - print nb to hexa
  * @nb: nb to print
@@ -11,8 +29,10 @@ int	ft_putshexa(unsigned long nb)
 {
 	char *base;
 	int i;
+	int j;
 	char *p;
 
+	j = len2(nb);
 	base = "0123456789ABCDEF";
 	i = 2;
 	p = (char *)malloc(sizeof(char) * (i + 1));
@@ -24,7 +44,8 @@ int	ft_putshexa(unsigned long nb)
 		p[i--] = base[nb % 16];
 		nb /= 16;
 	}
-	p[0] = '0';
+	if (j == 1)
+		p[0] = '0';
 	i = 0;
 	while (p[i])
 		write(1, &p[i++], 1);
